ec5a1641b30c8717b06ea010c726b981e5be851f
slayerfung <slayerfung@icloud.com>
2026-06-15 22:56:49 +0800

    personal cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,67 +1,66 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Fung Yiu Chung',
+  title: 'Business Analyst, IT Professional, Data Analyst',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Location': '<a href="https://maps.app.goo.gl/PHa6FFCRuGaojAFA6"><i class="fa fa-home fact-icon"></i>Hong Kong</a>',
+    'Email': '<a href="mailto:slayerfung@icloud.com"><i class="fa fa-envelope fact-icon"></i>slayerfung@icloud.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 60],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 80],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['SQL', 100],
+    ['HTML', 100],
+    ['CSS', 100],
+    ['JavaScript', 100],
+    ['Python', 100],
+    ['VMware', 100],
+    ['Word', 100],
+    ['Excel', 100],
+    ['PowerPoint', 80],
+    ['Access', 100],
+    ['IBM TM1', 100],
+	['Power BI', 100],
+	['Networking', 100],
+	['VBA', 100],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: 'IT Officer (Part-time), EFCC Waterloo Hill Church Limited',
+      period: 'Sept 2023 - May 2025',
+      skills: ['Website Development', 'Identity and Access Management', 'UAT', 'System Upgrade'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Managed the transition of legacy systems, moving the organization from aging infrastructure to modernized Enterprise Storage Solutions while ensuring Security & Governance remained the top priority.
+Bridged technical gaps by producing clear Technical Documentation and leading UAT for new CMS platforms, ensuring every deployment was reliable and user ready.
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'Information Manager - Financial Department, Bank of China (HK) Limited',
+      period: 'July 2017– July 2023',
+      skills: ['Data Management', 'Data Analysing', 'MIS'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Governed the end-to-end Data Management lifecycle, enforcing Data Standardization across the group to ensure 100% compliance with local and overseas regulatory standards. Modernized Schema Design for the bank’s MIS reporting platform. Optimized Information System Management by automating ETL.
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'Assistant Manager - Mortgage Insurance Department, The Hong Kong Mortgage Corporation Limited',
+      period: 'Oct 2003 – Dec 2016',
+      skills: ['Change Management', 'Workflow Optimisation'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+ Championed Change Management during the rollout of the Reverse Mortgage Programme, successfully managing the transition for both internal staff and external banking partners. Optimised Workflows by redesigning quality assurance practices, making the delivery of the Mortgage Insurance Programme more efficient and scalable. Optimised Workflows by redesigning quality assurance practices, making the delivery of the Mortgage Insurance Programme more efficient and scalable.
 `
     }
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: 'Languages',
+      skills: ['language'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+ Cantonese, English, Mandarin
 `
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      header: 'Expected Salary',
+      skills: ['Salary'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
+    asked to provide
 `
     }
   ]
